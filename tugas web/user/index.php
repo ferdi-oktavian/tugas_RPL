a830a73626ce52dd7c876164771a6c50c5460f84
mraihanaditya <153346900+mraihanaditya@users.noreply.github.com>
2025-01-09 00:38:32 +0700

    Update index.php

--- tugas web/user/index.php
+++ tugas web/user/index.php
@@ -1,17 +1,5 @@
 <?php
-// Database connection (replace with your own credentials)
-$servername = "localhost";  // Your database server
-$username = "root";         // Your database username
-$password = "";             // Your database password
-$dbname = "ajra";  // Your database name
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include '../koneksi/koneksi.php';
 
 // SQL query to get data from the images table
 $sql = "SELECT id, name, image FROM images";
