0d9b92c9b4f2ec82c88e074e76b192fa9f83c568
aldikriewan <115218898+aldikriewan@users.noreply.github.com>
2025-01-09 11:12:15 +0700

    Update script.sql

--- tugasweb/koneksi/script.sql
+++ tugasweb/koneksi/script.sql
@@ -94,5 +94,39 @@ CREATE TABLE users (
 
 
 -- --------------------------------------------------------
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `name`, `image`) VALUES
+(1, 'asdasdasdfaf', 'asu'),
+(2, 'qweqweqwe', 'weq'),
+(3, 'dsfgkjbsdg', 'sodjfbnnsdgf');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
+
