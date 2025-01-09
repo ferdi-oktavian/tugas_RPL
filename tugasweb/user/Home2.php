ed07a2818eab828e8c51f011386de4ac890a6160
ferdy <muhammadferdisp33@gmail.com>
2025-01-09 20:29:30 +0700

    apa aja

--- tugasweb/user/Home2.php
+++ tugasweb/user/Home2.php
@@ -11,7 +11,7 @@ $result = $conn->query($sql);
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sahabat Sakit</title>
+    <title>Sahabat Sehat</title>
 
     <!-- Style -->
     <link rel="stylesheet" href="../css/style.css" />
