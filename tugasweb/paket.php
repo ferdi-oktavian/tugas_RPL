8f2c200de99e710e923853440511910849f3a931
Alwan <fadhilalwan902@gmail.com>
2025-01-13 08:11:42 +0700

    sss

--- tugasweb/paket.php
+++ tugasweb/paket.php
@@ -22,6 +22,7 @@
             gap: 20px;
             padding: 20px 40px;
             cursor: grab;
+            
         }
 
         .carousel:active {
@@ -41,6 +42,7 @@
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
             margin-bottom: 20px;
             position: relative;
+            background-color: rgb(129, 213, 202);
         }
 
 
